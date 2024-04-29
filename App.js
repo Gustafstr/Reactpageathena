25b22db482296caf2a9c500a783767a21ca7670b
Gustafstr <gustafstrausss@gmail.com>
2024-04-29 16:06:01 +0200

    överföring av kod

--- App.js
+++ App.js
@@ -1,24 +1,132 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./index.css";
+import groupImage from "./images/Group 210.png";
+import movilImage from "./images/movil.png";
+import AthenaImage from "./images/Athena.png";
+import bookstoreImage from "./images/bookstore.png";
+import React, { useState, useCallback } from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <header className="header">
+        <img src={AthenaImage}></img>
+      </header>
+    </div>
+  );
+}
+
+function MainContent() {
+  return (
+    <div className="main">
+      <h2 className="subtitle">
+        Streamline your studies{" "}
+        <span className="green-text">go digital with Athena</span>
+      </h2>
+      <p className="content">
+        We give students a new way to study. Digitally. Find your e-coursebooks,
+        documents, and past-exams all in one place. Cheaper than physical books.
+        Smarter than physical books. Join Athena.
+      </p>
+      <div className="images-container">
+        <img
+          alt="Image description needed"
+          src={groupImage}
+          className="Dator"
+        />
+        <img src={movilImage} className="movil" />
+      </div>
+      <h5>
+        <span className="green-text">Hey! </span> We are launching soon, join
+        our waitlist and get earlybird discount!
+      </h5>
+      <SignupForm />
+      <img
+        src={bookstoreImage}
+        alt="Additional image description needed"
+        className="bookstore"
+      />
+      <h5>Are you a publisher wanting to sell your books through Athena?</h5>
+      <PublisherButton />
+    </div>
+  );
+}
+
+function SignupForm() {
+  const [email, setEmail] = useState("");
+
+  const handleChange = useCallback((e) => {
+    setEmail(e.target.value.trim());
+  }, []);
+
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      if (email) {
+        fetch(`/api/memberAdd?email=${email}`) // Use the email state directly
+          .then((res) => res.json())
+          .then((res) => console.log(res))
+          .catch((err) => console.log(err));
+      }
+    },
+    [email]
+  );
+  return (
+    <form className="email-form" onSubmit={handleSubmit}>
+      <input
+        type="email"
+        placeholder="Your Email Address"
+        className="email-input"
+        onChange={handleChange}
+        value={email}
+      />
+      <button type="submit" className="yes-button">
+        Yes Please!
+      </button>
+    </form>
+  );
+}
+
+function PublisherButton() {
+  return <button className="transparent-button">Click here</button>;
+}
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-section contact">
+          <h3>Contact</h3>
+          <p>
+            Get in contact with the team, send us an email at:{" "}
+            <a href="mailto:hey@athena.se">hey@athena.se</a>
+          </p>
+        </div>
+        <div className="footer-section find-us">
+          <h3>Where to find us?</h3>
+          <ul>
+            <li>Instagram</li>
+            <li>LinkedIn</li>
+            <li>Artillerigatan 27, Stockholm, Sweden</li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer-branding">
+        <img src={AthenaImage} alt="Athena logo" className="footer-logo" />
+        <p>@Athena EdTech AB 2024</p>
+      </div>
+    </footer>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
   );
 }
 
